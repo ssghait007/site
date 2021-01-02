ab764640448d01b7e84a7dfa3634f78c73013707
ssghait007 <ssghait.007@gmail.com>
2021-01-02 17:14:29 +0530

    ✨ create initial base

---
+++ layouts/default.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app>
+      <span
+        >&copy; {{ new Date().getFullYear() }} Author: Sachin Ghait
+        <a href="mailto:ssghait.007@gmail.com">ssghait.007@gmail.com</a>
+      </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/',
+        },
+        {
+          icon: 'mdi-dialpad',
+          title: 'WorK Experience',
+          to: '/projects',
+        },
+        {
+          icon: 'mdi-face',
+          title: 'About me',
+          to: '/aboutme',
+        },
+        {
+          icon: 'mdi-email',
+          title: 'Contact',
+          to: '/contact',
+        },
+      ],
+      miniVariant: false,
+      title: 'Sachin Ghait',
+    }
+  },
+}
+</script>
